edcd616ad1d14a9e6d86be933721ebd226a9b0dc
Mircio <mfsn@cesar.school>
2025-03-25 23:47:31 -0300

    2253 beencword funcionando

--- 02 lista/2253.c
+++ 02 lista/2253.c
@@ -3,7 +3,7 @@
 
 int main(){
 
-    char senha[33];
+    char senha[120];
     
     while(gets(senha) != NULL){
 
@@ -11,9 +11,12 @@ int main(){
 
         int valido=1;
 
-        if(tamanho<6 || tamanho>32) valido=0;
-
-
+        if(tamanho<6 || tamanho>32){
+            printf("Senha invalida.\n");
+            continue;
+            
+        }
+        
         int maisculo=0;
         int minusculo=0;
         int numero=0;
@@ -26,34 +29,17 @@ int main(){
 
             if(senha[i]>=48 && senha[i]<=57) numero++;
 
-            if(senha[i]<=47) valido=0;
-
-            if(senha[i] >=58 && senha[i]<=64) {
-                valido=0;
-                break;
-            }
-
-            if(senha[i] >=91 && senha[i]<=96){
-                valido=0;
-                break;
-            }
-
-            if(senha[i]>= 123) {
-                valido=0;
+            if (senha[i] <= 47 || (senha[i] >= 58 && senha[i] <= 64) || (senha[i] >= 91 && senha[i] <= 96) || senha[i] >= 123) {
+                valido = 0;
                 break;
             }
 
         }
-        
+      
         if(maisculo==0 || minusculo==0 || numero==0) valido=0;
 
-
-
-        
-
-        
-
         if(valido) printf("Senha valida.\n");
+
         else printf("Senha invalida.\n");
 
     }
