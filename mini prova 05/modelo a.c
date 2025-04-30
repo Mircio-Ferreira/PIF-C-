ea6b7bfae693b02393fd345396b65ec03332089b
Mircio-Ferreira <mfsn@cesar.school>
2025-04-29 22:19:04 -0300

    enviando todas as provas

--- mini prova 05/modelo a.c
+++ mini prova 05/modelo a.c
@@ -4,3 +4,86 @@
 
 //Estrutura do aluno com base nos dados descritos na questão (0.5);
 //Função de inserção de alunos em ordem alfabética de nome (1.0);
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+    char name[51];
+    int falta;
+    float vetor[10];
+    struct node *next;
+}no;
+
+void insert_sort(no **head,char *nome,int ft,float *ptr_vecotr);
+
+void print_list(no *head);
+
+
+int main(){
+    
+    no *head = NULL;
+    float notas1[10] = {7.0, 8.0, 9.0, 7.5, 6.5, 8.0, 7.0, 9.5, 8.0, 7.0};
+    float notas2[10] = {6.0, 7.0, 8.0, 7.0, 8.5, 7.0, 6.5, 7.5, 8.0, 9.0};
+    float notas3[10] = {8.0, 9.0, 7.5, 8.0, 9.5, 8.5, 9.0, 9.0, 8.0, 8.5};
+
+    insert_sort(&head, "Carlos", 3, notas1);
+    insert_sort(&head, "Ana", 2, notas2);
+    insert_sort(&head, "Beatriz", 1, notas3);
+
+    print_list(head);
+
+    return 0;
+}
+
+void insert_sort(no **head,char *nome,int ft,float *ptr_vecotr){
+    no *new=malloc(sizeof(no));
+
+    strcpy(new->name,nome);
+    new->falta=ft;
+    for(int i=0;i<10;i++){
+        new->vetor[i]=ptr_vecotr[i];
+    }
+    new->next=NULL;
+
+    if(*head==NULL) *head=new;
+
+    else if(strcmp(nome,(*head)->name) <0 ){
+        new->next=*head;
+        *head=new;
+    }
+
+    else{  
+        no *aux=*head;
+
+        while(aux!=NULL){
+
+            if(aux->next!=NULL &&  strcmp(nome,aux->next->name) < 0 ){
+                new->next=aux->next;
+                aux->next=new;
+                return;
+            }
+
+            aux=aux->next;
+        }
+        // Se sair do while que dizer que vou inserir no ultimo
+        aux->next=new;
+
+    }
+
+}
+
+void print_list(no *head) {
+    no *aux = head;
+    while (aux != NULL) {
+        printf("Nome: %s, Faltas: %d\n", aux->name, aux->falta);
+        printf("Notas: ");
+        for (int i = 0; i < 10; i++) {
+            printf("%.2f ", aux->vetor[i]);
+        }
+        printf("\n\n");
+        aux = aux->next;
+    }
+}
