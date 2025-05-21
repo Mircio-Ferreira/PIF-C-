30fe6f7a7594ddc16a00b5ea4fba5e23869f8466
Mircio-Ferreira <mfsn@cesar.school>
2025-05-20 21:07:01 -0300

    enviando parte daquestao do beencrowd

--- 05 lista/1030.c
+++ 05 lista/1030.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(){
 
+
+int main(){
+    int loops;
+
+    scanf("%d",&loops);
+
+    for(int i=0;i<loops;i++){
+        int pulos;
+        scanf("%d",&pulos);
+
+        printf("Case %d: %d",i+1,);
+    }
     
     return 0;
 }
